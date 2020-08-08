9ea1b453b1c804ed5d5f6f4c88183e8c4b53841c
alicialycan <alicialycan@gmail.com>
2020-08-08 14:50:52 -0700

    add route to delete comment by id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -97,8 +97,6 @@ router.delete('/:id', auth, async (req, res) => {
   }
 });
 
-// TODO: Add likes
-
 // @route   PUT api/posts/like/:id
 // @desc    Like a post
 // @access  Private
@@ -139,6 +137,77 @@ router.put('/unlike/:id', auth, async (req, res) => {
 
     await post.save();
     res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route   POST api/posts/comment/:id
+// @desc    Comment on a post
+// @access  Private
+
+router.post('/comment/:id', 
+  [
+    auth,
+    [
+      check('text', 'Text is required').not().isEmpty()
+    ]
+  ], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+    const user = await User.findById(req.user.id).select('-password');
+    const post = await Post.findById(req.params.id);
+
+    const newComment = {
+      text: req.body.text,
+      name: user.name,
+      avatar: user.avatar,
+      user: req.user.id
+    };
+
+    // NOTE: Add comment to the beginning of comments
+    post.comments.unshift(newComment);
+
+    await post.save();
+
+    res.json(post.comments);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  });
+
+// @route   DELETE api/posts/comment/:id/comment/:id
+// @desc    Delete comment
+// @access  Private
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // NOTE: Pull out comment
+    const comment = post.comments.find(comment => comment.id === req.params.comment_id);
+    console.log(comment);
+    // NOTE: Check comment exists
+    if (!comment) {
+      return res.status(404).json({ msg: 'Comment does not exist' });
+    }
+    // NOTE: Check user 
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+    // NOTE: Get remove index
+    const removeIndex = post.comments.map(comment => comment.user.toString()).indexOf(req.user.id);
+
+    // NOTE: Remove post from array
+    post.comments.splice(removeIndex, 1);
+
+    await post.save();
+    res.json(post.comments);
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');
