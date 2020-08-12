95f7b9bc3511f8ed054ac6d2f9a0c7ab54d311fc
alicialycan <alicialycan@gmail.com>
2020-08-12 14:40:01 -0700

    add profile form

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -1,4 +1,5 @@
 import React, { useState, Fragment } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
@@ -34,6 +35,14 @@ const CreateProfile = props => {
     youtube,
     instagram
   } = formData;
+
+  const onChange = e =>
+  setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+  };
 
   return (
     <Fragment>
