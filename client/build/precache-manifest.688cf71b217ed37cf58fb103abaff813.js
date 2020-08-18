5c47c1b4d6e63e8e3da77fbbc51f66dce8b35af3
alicialycan <alicialycan@gmail.com>
2020-08-18 10:22:38 -0700

    ready to deploy

---
+++ client/build/precache-manifest.688cf71b217ed37cf58fb103abaff813.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0933cdc4d6a494593c3c7d855389d49c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c800a3b5c65228b56236",
+    "url": "/static/css/main.28ec31e2.chunk.css"
+  },
+  {
+    "revision": "8f77005c9e943d456dda",
+    "url": "/static/js/2.dbdf6981.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.dbdf6981.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c800a3b5c65228b56236",
+    "url": "/static/js/main.6d9023c5.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
